ecf4c8ea415b2a3d69a19ec5beec0b9cd647cef4
Hanzhi Yin <yinhanzhi@gmail.com>
2022-12-18 13:07:19 -0500

    finished baseconfig. need testing

---
+++ base_config/base_config.py
@@ -0,0 +1,215 @@
+import json
+import os
+from abc import ABC
+from argparse import ArgumentParser
+from dataclasses import MISSING, asdict, dataclass, fields
+from decimal import Decimal
+from fractions import Fraction
+from pathlib import Path
+from pprint import pprint
+from types import UnionType
+from typing import Any, Literal, Union, get_args, get_origin
+
+__all__ = ['BaseConfig']
+
+_supported_singleton_types = [
+    int, Fraction, Decimal, float, complex, str, Path, bool,
+]
+"""Supported singleton types.
+These types are not container types,
+and they all accepts a single string for construction.
+"""
+
+_supported_types = [
+    # Sequential type with fixed values. All choices must have the same type.
+    Literal,
+    # Sequential type with non_fixed values. Only one type can exist.
+    list,
+] + _supported_singleton_types
+"""All supported types in `BaseConfig` class"""
+
+
+_JSON_CUSTOM_TYPE_KEY = '__custom_type__'
+_JSON_CUSTOM_TYPE_VALUE = '__value__'
+
+
+class BaseConfigJSONEncoder(json.JSONEncoder):
+    def default(self, o: Any) -> Any:
+        if isinstance(o, complex):
+            return {
+                _JSON_CUSTOM_TYPE_KEY: 'complex',
+                _JSON_CUSTOM_TYPE_VALUE: str(o)
+            }
+        elif isinstance(o, Decimal):
+            return {
+                _JSON_CUSTOM_TYPE_KEY: 'Decimal',
+                _JSON_CUSTOM_TYPE_VALUE: str(o)
+            }
+        elif isinstance(o, Fraction):
+            return {
+                _JSON_CUSTOM_TYPE_KEY: 'Fraction',
+                _JSON_CUSTOM_TYPE_VALUE: str(o)
+            }
+        elif isinstance(o, Path):
+            return {
+                _JSON_CUSTOM_TYPE_KEY: 'Path',
+                _JSON_CUSTOM_TYPE_VALUE: str(o)
+            }
+        return super().default(o)
+
+
+def base_config_json_decode_object_hook(dct: dict[str, Any]):
+    if (
+        _JSON_CUSTOM_TYPE_KEY in dct and
+        _JSON_CUSTOM_TYPE_VALUE in dct and
+        len(dct) == 2
+    ):
+        key = dct[_JSON_CUSTOM_TYPE_KEY]
+        value = dct[_JSON_CUSTOM_TYPE_VALUE]
+        if key == 'complex':
+            return complex(value)
+        elif key == 'Decimal':
+            return Decimal(value)
+        elif key == 'Fraction':
+            return Fraction(value)
+        elif key == 'Path':
+            return Path(value)
+    return dct
+
+
+@dataclass(init=True)
+class BaseConfig(ABC):
+    @classmethod
+    def from_dict(cls, from_dict: dict[str, Any]):
+        return cls(**from_dict)
+
+    @classmethod
+    def from_json(cls, json_file: os.PathLike):
+        with open(json_file, 'r') as f:
+            incoming_data = json.load(
+                f, object_hook=base_config_json_decode_object_hook
+            )
+        return cls.from_dict(incoming_data)
+
+    @classmethod
+    def from_program_arguments(
+        cls, arguments: list[str] | None = None,
+        parser: ArgumentParser | None = None,
+    ):
+        if parser is None:
+            parser = ArgumentParser(
+                prog=cls.__name__,
+                description=cls.__doc__,
+            )
+        args = parser.parse_args(arguments)
+        return cls.from_dict(vars(args))
+
+    @classmethod
+    def forge_argument_parser_named_options(cls):
+        for field in fields(cls):
+            field_type = field.type
+            field_name = field.name
+            field_default_value = field.default
+
+            arg_name = '--' + field_name.replace('_', '-') \
+                if field.kw_only or get_origin(field_type) is list \
+                else field_name
+
+            arg_options = dict()
+            if field_default_value != MISSING:
+                arg_options['default'] = field_default_value
+                arg_options['required'] = False
+            else:
+                arg_options['required'] = True
+
+            if get_origin(field_type) is list:
+                arg_options['nargs'] = '*'
+                arg_options['type'] = get_args(field_type)[0]
+            elif get_origin(field_type) is Literal:
+                arg_options['choices'] = get_args(field_type)
+            else:
+                arg_options['type'] = field_type
+
+            yield arg_name, arg_options
+
+    def __post_init__(self):
+        self._validate_instance_variable_types()
+
+    def to_dict(self):
+        return asdict(self)
+
+    def to_json(self, json_file: os.PathLike):
+        with open(json_file, 'w') as f:
+            json.dump(self.to_dict(), f, cls=BaseConfigJSONEncoder)
+
+    def _validate_instance_variable_types(self):
+        for field in fields(self):
+            pprint(f'{field = }')
+            field_name = field.name
+            field_type = field.type
+
+            try:
+                field_val = getattr(self, field_name)
+            except AttributeError:
+                raise ValueError(
+                    f'`{field_name}` expects a value, but nothing is provided.'
+                )
+
+            if field_type in _supported_singleton_types:
+                if not isinstance(field_val, field_type):
+                    raise TypeError(
+                        f'`{field_name}` is expected to be a(n) `{field_type}`'
+                        f' but got {type(field_val)}.'
+                    )
+            elif get_origin(field_type) is Literal:
+                literal_args = get_args(field_type)
+                literal_types = list(map(type, get_args(field_type)))
+                for i in range(len(literal_types) - 1):
+                    if literal_types[i] is not literal_types[i + 1]:
+                        raise TypeError(
+                            f'Expects all choices in a `Literal` type to have '
+                            f'the same type, but found inconsistent members '
+                            f'`{literal_args[i]}` with type '
+                            f'`{literal_types[i]}` and '
+                            f'`{literal_args[i + 1]}` with type '
+                            f'`{literal_types[i + 1]}`.'
+                        )
+                if field_val not in get_args(field_type):
+                    raise TypeError(
+                        f'`{field_val}` is not one of `{literal_args}`.'
+                    )
+            elif get_origin(field_type) is list:
+                list_args = get_args(field_type)
+                if len(list_args) != 1:
+                    raise TypeError(
+                        f'Expected only one member type in list, '
+                        f'but found {list_args}.'
+                    )
+                list_type = list_args[0]
+                if (
+                    get_origin(list_type) is Union or
+                    get_origin(list_type) is UnionType or
+                    get_origin(list_type) is Literal
+                ):
+                    raise TypeError(
+                        f'`list` doesn\' t support `Union` or `Literal` types.'
+                    )
+                elif list_type not in _supported_singleton_types:
+                    raise TypeError(
+                        f'Expect the list to have one of '
+                        f'`{_supported_singleton_types}` '
+                        f'but found `{list_type}`.'
+                    )
+
+                if not isinstance(field_val, list):
+                    raise TypeError(
+                        f'`{field_name}` is expected to be a list, '
+                        f'but got {type(field_val)}. '
+                    )
+                for v in field_val:
+                    if not isinstance(v, list_type):
+                        raise TypeError()
+            else:
+                raise TypeError(
+                    f'`{field_type}` is not supported by `BaseConfig`.'
+                )
---
+++ tests/type_checking.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestTypeChecking(TestCase):
+    def test_numerical_type(self):
+        self.assertEqual(2, 2)
