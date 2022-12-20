6eb2a71d1f179bcdd84e2cfd6721bbc19580ba24
Hanzhi Yin <yinhanzhi@gmail.com>
2022-12-20 17:24:13 -0500

    preparing for PyPI submission

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 
+
 setup(
-    name='baseconfig',
-    version='0.0.1alpha',
+    name='rootconfig',
+    version='0.1.0',
     author='Hanzhi Yin',
-    author_email='yinhanzhi@gmail.com'
+    author_email='yinhanzhi@gmail.com',
+    where='rootconfig'
 )
